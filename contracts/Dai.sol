12a4a47bd9c366d5622f83b56fc4404e35f5688e
ra0926 <134577104+ra0926@users.noreply.github.com>
2023-06-08 01:19:30 +0900

    Deployed Address

--- contracts/Dai.sol
+++ contracts/Dai.sol
@@ -1,15 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
 
 contract Dai is Ownable, ERC20 {
-    constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {
+  constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {}
 
-    }
-
-    function mint(address to, uint256 amount) external onlyOwner {
-        _mint(to, amount);
-    }
+  function mint(address to, uint256 amount) external onlyOwner {
+    _mint(to, amount);
+  }
 }
